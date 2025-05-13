25ad58844889c23256458c481ac6d17c5b9bdb99
yyLU000 <152509830+yyLU000@users.noreply.github.com>
2025-05-13 15:16:57 +0800

    Update gc_write.sh

--- GC/gc_write.sh
+++ GC/gc_write.sh
@@ -30,26 +30,37 @@ sleep 1
 echo "[+] Starting sequential write test..."
 fio --name=seq_write \
     --filename=$FILE \
-    --size=4G \
+    --size=3584M \
     --rw=write \
     --bs=128K \
     --ioengine=sync \
     --numjobs=1 \
-    --runtime=60 \
     --direct=1 \
     --group_reporting
 
 echo "[+] Starting random update test..."
 fio --name=rand_update \
     --filename=$FILE \
-    --size=1G \
+    --size=1500M \
     --rw=randwrite \
     --bs=4K\
     --ioengine=sync \
     --numjobs=4 \
-    --runtime=300 \
+    --runtime=120 \
     --direct=1 \
     --time_based \
+    --group_reporting
+
+echo "[+] Starting append write test..."
+fio --name=append_write \
+    --filename=$FILE \
+    --size=716M \
+    --offset=3584M \
+    --rw=write \
+    --bs=128K \
+    --ioengine=sync \
+    --numjobs=1 \
+    --direct=1 \
     --group_reporting
 
 echo "[+] Stopping trace_pipe logging..."
