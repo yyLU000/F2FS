bf9f9584a7a30c4ccc8edf9136973ae09f2cac8b
yyLU000 <152509830+yyLU000@users.noreply.github.com>
2025-05-27 17:05:37 +0800

    Update gc_multi_read.sh

--- GC/gc_multi_read.sh
+++ GC/gc_multi_read.sh
@@ -2,6 +2,7 @@ MOUNT_POINT="/mnt/f2fs-test"
 DEVICE="/dev/vdb"  
 FILE="$MOUNT_POINT/test1"
 NUM_FILES=4
+RUNTIME=30
 
 df -h $MOUNT_POINT
 
@@ -9,21 +10,22 @@ echo "[+] Dropping caches before read test..."
 sync
 echo 3 > /proc/sys/vm/drop_caches
 
+echo "[+] Count Read I/O..."
+cat /sys/block/vdb/stat
+
 echo "[+] Starting sequential read test after fragmentation..."
 
-for i in $(seq 1 $NUM_FILES); do
-  fio --name=seq_read_$i \
-      --filename=${FILE_PREFIX}_$i \
-      --size=512M \
-      --rw=read \
-      --bs=128K \
-      --ioengine=libaio \
-      --iodepth=16 \
-      --numjobs=1 \
-      --direct=1 \
-      --runtime=60 \
-      --group_reporting &
-done
+fio --name=seq_read_large \
+    --directory="$MOUNT_POINT" \
+    --filename_format='testfile_0' \
+    --filesize=512M \
+    --rw=read \
+    --bs=128K \
+    --ioengine=sync \
+    --numjobs=1 \
+    --direct=1 \
+    --group_reporting \
+    --thread
 
 wait
 sync
